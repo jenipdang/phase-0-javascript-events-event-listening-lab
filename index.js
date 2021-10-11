84c1d15969e1ad635b0d500fe9688273f1d9105d
Jeni Dang <jenipdang@yahoo.com>
2021-10-10 18:36:42 -0700

    commit tree bfd1914df8b5a31fa985de1a93e9777b452e2869

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// const input = document.getElementById('input')
+
+// function addingEventListener() {
+//     alert('Hee hee, that tickles!')
+// }
+
+// input.addEventListener('click', addingEventListener)
+
+const input = document.getElementById('input')
+
 function addingEventListener() {
+    input.addEventListener('click', function(){
+        alert('I was clicked!')
+    })
 }
+
+addingEventListener();
+
